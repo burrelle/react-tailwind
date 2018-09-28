b944f2800d463f5d5fd416730db3e9d165f5555e
Evan Burrell <burrelle@oregonstate.edu>
2018-09-28 16:31:58 -0700

    Initial

---
+++ README.md
@@ -0,0 +1,8 @@
+# React Tailwind Starter
+
+> A starter application built with React, TailwincCSS and Parcel
+
+1. `git clone` the repository
+2. `yarn install` the dependencies
+3. `yarn start`
+4. Visit `localhost:1234`
---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+var tailwindcss = require('tailwindcss');
+
+module.exports = {
+    plugins: [
+        // ...
+        tailwindcss('./path/to/your/tailwind-config.js'),
+        require('autoprefixer')
+        // ...
+    ]
+};
